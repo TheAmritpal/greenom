11babddc56c9ba1e0a5730555737377b4e21ee2a
theAmritpal <amritpal@hotmail.co.in>
2022-06-19 23:08:18 +0530

    with video

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,16 +20,26 @@ $(".rotate").click(function () {
 });
 
 $(".img-div").on("click", function () {
-  var alldiv = $(".img-div").find("div");
-  alldiv.each(function (key, item) {
-    if (!$(item).hasClass("overlay")) {
-      $(item).addClass("overlay");
-    }
-  });
+  $(".img-div")
+    .find("div")
+    .each(function (key, item) {
+      if (!$(item).hasClass("overlay")) {
+        $(item).addClass("overlay");
+      }
+    });
 
   if ($(this).find("div.overlay").length == 1) {
     $(this).find("div.overlay").removeClass("overlay");
   }
   var path = $(this).find("img").attr("src");
-  $(".img").attr("src", path);
+  var video = $(this).find("img").attr("is-video");
+  var videopath = $(this).find("img").attr("video-src");
+  if (video != undefined && video == 1) {
+    $(".video").removeClass("d-none");
+    $(".img").addClass("d-none");
+    $(".video").html(`<source src="${videopath}" type="video/mp4"></source>`);
+  } else {
+    $(".video").addClass("d-none");
+    $(".img").removeClass("d-none").attr("src", path);
+  }
 });
